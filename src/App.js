516e49cf42e8a4398e15da89a46d84c70c4b450d
MYRDDINJ41 <ajaimes.acjs@gmail.com>
2023-05-24 11:19:41 -0500

    otro commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import storageRoutes from "./routes/Storage.routes.js"
+import cors from 'cors';
 
 
 const app = express();
 
 app
+    .use(cors())
     .use(express.json())
     .use("/api", storageRoutes)
     .use((req, res, next) => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { PORT } from './model/Config.js';
 import app from './App.js';
 
+
 app.listen(PORT)
 console.log("server listening on port " + PORT);
