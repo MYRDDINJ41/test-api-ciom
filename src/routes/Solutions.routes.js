e395e854f6e3a480fe1845a01bf455a45e778ad8
MYRDDINJ41 <ajaimes.acjs@gmail.com>
2023-05-31 09:19:53 -0500

    cimmit

--- src/routes/Solutions.routes.js
+++ src/routes/Solutions.routes.js
@@ -5,8 +5,10 @@ import { getSolutions, createSolution, deleteSolution, updateSolution, getSoluti
 const router = Router();
 
 router
+  
   .get("/solutions", getSolutions)
   .get("/solution/:id", getSolutionId)
+
   .post("/create-solutions", createSolution)
   .delete("/delete-solution/:id", deleteSolution)
   .patch("/update-solution/:id", updateSolution);
