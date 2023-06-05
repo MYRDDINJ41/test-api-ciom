5cd8b929e5c153bb33f50806ff750af2b8c9e713
MYRDDINJ41 <ajaimes.acjs@gmail.com>
2023-06-04 19:27:22 -0500

    otro

--- src/controller/Solutions.controller.js
+++ src/controller/Solutions.controller.js
@@ -100,9 +100,10 @@ export const uploadImage = async (req, res) => {
     const result = await db.query("UPDATE solution_ciom SET img_solution = IFNULL(?, img_solution) WHERE id_solution = ?", [imgUrl, req.params.id]);
 
     console.log(result);
+    console.log(req.file)
     //res.send(result);
 
-    
+
 
     const [rows] = await db.query("SELECT * FROM solution_ciom WHERE id_solution = ?",[req.params.id]);
 
